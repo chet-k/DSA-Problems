e40f365b8e18f289a279d4f099a42a0899f177b4
chet-k <chetkupchella@gmail.com>
2021-05-26 11:45:47 -0400

    reorganize string LC 767

--- reorganize_string.rb
+++ reorganize_string.rb
@@ -3,16 +3,16 @@ def reorganize_string(s)
     counts = Hash.new(0)
     s.each_char {|char| counts[char] += 1}    
     
-	# check whether reorg. is impossible
-	n = counts.values.max
+    # check whether reorg. is impossible
+    n = counts.values.max
     return out unless (s.length + 1) / 2 >= n
     
-	# Generate string sorted by char count 
+    # Generate string sorted by char count 
     sorted_s = ""
-	sorted_counts = counts.sort_by {|k, v| -v}
+    sorted_counts = counts.sort_by {|k, v| -v}
     sorted_counts.each {|k, v| sorted_s += k * v}
     
-	# Generate reorganized string, alternating picks from first and second half of s
+    # Generate reorganized string, alternating picks from first and second half of s
     p1 = 0
     p2 = s.length / 2
     p2 += 1 unless s.length.even?
