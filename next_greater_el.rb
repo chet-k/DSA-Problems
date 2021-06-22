03755b7ade73fee9b465d2c7589236aeee140b6b
chet-k <chetkupchella@gmail.com>
2021-06-22 09:58:20 -0400

    next greater el II

--- next_greater_el.rb
+++ next_greater_el.rb
@@ -1,34 +1,5 @@
-# @param {Integer[]} nums1
-# @param {Integer[]} nums2
-# @return {Integer[]}
-def next_greater_element(nums1, nums2)
-    ans = [-1] * nums1.length
-    
-    # brute force: for each nums1, 
-    #   1. scan nums2 to find element
-    #   2. continue scanning nums2 until greater element 
-    #       found (add to ans), or end of nums2 (add -1 to ans)
-    
-    # stack:
-    #   1. 1 pass thru nums1, make a hash s.t. h[n] = i1 where n is element and i1 is index in nums1. 
-    #   2. 1 pass thru nums2, push element to stack if empty. if next element is smaller push to stack. on greater element, pop until empty, putting the found element into ans at indices for each el in the stack.
-    
-    h = {}
-    nums1.each_with_index {|n, i1| h[n] = i1}
-    
-    stack = []
-    nums2.each do |n|
-        if !stack.empty? && n > stack[-1]
-            until stack.empty? || n < stack[-1]
-                ni = stack.pop
-                ans[h[ni]] = n
-            end
-        end
-        stack << n if h.keys.include?(n)
-    end
-    ans
-end
-'''
+"""
+NEXT GREATER ELEMENT I
 The next greater element of some element x in an array is the first greater element that is to the right of x in the same array.
 
 You are given two distinct 0-indexed integer arrays nums1 and nums2, where nums1 is a subset of nums2.
@@ -62,4 +33,79 @@ Constraints:
 0 <= nums1[i], nums2[i] <= 104
 All integers in nums1 and nums2 are unique.
 All the integers of nums1 also appear in nums2.
-'''
+"""
+# @param {Integer[]} nums1
+# @param {Integer[]} nums2
+# @return {Integer[]}
+def next_greater_element_i(nums1, nums2)
+    ans = [-1] * nums1.length
+    
+    # brute force: for each nums1, 
+    #   1. scan nums2 to find element
+    #   2. continue scanning nums2 until greater element 
+    #       found (add to ans), or end of nums2 (add -1 to ans)
+    
+    # stack:
+    #   1. 1 pass thru nums1, make a hash s.t. h[n] = i1 where n is element and i1 is index in nums1. 
+    #   2. 1 pass thru nums2, push element to stack if empty. if next element is smaller push to stack. on greater element, pop until empty, putting the found element into ans at indices for each el in the stack.
+    
+    h = {}
+    nums1.each_with_index {|n, i1| h[n] = i1}
+    
+    stack = []
+    nums2.each do |n|
+        if !stack.empty? && n > stack[-1]
+            until stack.empty? || n < stack[-1]
+                ni = stack.pop
+                ans[h[ni]] = n
+            end
+        end
+        stack << n if h.keys.include?(n)
+    end
+    ans
+end
+
+"""
+NEXT GREATER ELEMENT II:
+Given a circular integer array nums (i.e., the next element of nums[nums.length - 1] is nums[0]), return the next greater number for every element in nums.
+
+The next greater number of a number x is the first greater number to its traversing-order next in the array, which means you could search circularly to find its next greater number. If it doesn't exist, return -1 for this number.
+
+Example 1:
+
+Input: nums = [1,2,1]
+Output: [2,-1,2]
+Explanation: The first 1's next greater number is 2; 
+The number 2 can't find next greater number. 
+The second 1's next greater number needs to search circularly, which is also 2.
+Example 2:
+
+Input: nums = [1,2,3,4,3]
+Output: [2,3,4,-1,4]
+ 
+Constraints:
+
+1 <= nums.length <= 10^4
+-10^9 <= nums[i] <= 10^9
+"""
+# @param {Integer[]} nums
+# @return {Integer[]}
+def next_greater_element_ii(nums)
+    ans = [-1] * nums.length
+    
+    stack = []
+    i = 0
+    
+    (nums.length*2).times do 
+        if !stack.empty? && nums[i] > nums[stack[-1]]
+            until stack.empty? || nums[i] <= nums[stack[-1]]
+                ans[stack.pop] = nums[i]
+            end
+        end
+        stack << i    
+        i += 1
+        i = 0 if i >= nums.length
+    end
+    
+    ans
+end
