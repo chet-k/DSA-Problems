fec12824ca35ae1c18cd8a48dc8c4ed683767e38
chet-k <chetkupchella@gmail.com>
2021-07-08 15:47:14 -0400

    remove duplicates from sorted array preserving orer linear time constant space

---
+++ remove_duplicates_sorted_arr_ii.rb
@@ -0,0 +1,91 @@
+"""
+80. Remove Duplicates from Sorted Array II
+https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
+
+Given an integer array nums sorted in non-decreasing order, remove some 
+duplicates in-place such that each unique element appears at most twice. 
+The relative order of the elements should be kept the same.
+
+Since it is impossible to change the length of the array in some languages, 
+you must instead have the result be placed in the first part of the array nums. 
+More formally, if there are k elements after removing the duplicates, then the 
+first k elements of nums should hold the final result. It does not matter what 
+you leave beyond the first k elements.
+
+Return k after placing the final result in the first k slots of nums.
+
+Do not allocate extra space for another array. You must do this by modifying 
+the input array in-place with O(1) extra memory.
+"""
+
+# @param {Integer[]} nums
+# @return {Integer}
+#
+# Optimization: what benefit do we get from increasing array? 
+#   this means when nums[i] != nums[i-1], there are no more duplicates of 
+#   nums[i-1] duplicates to the right. i.e. we don't need 
+#   a hash to keep track of duplicates, can do it with O(1) memory
+def remove_duplicates(nums)
+    dups = 0
+    
+    prev_num = nums[0]
+    cur_ct = 0
+    nums.each_with_index do |n, i|
+        if n == prev_num
+            cur_ct += 1
+        else
+            prev_num = n
+            cur_ct = 1
+        end
+
+        if cur_ct > 2
+            dups += 1
+        else
+            nums[i - dups] = n
+        end
+    end
+    
+    nums.length - dups
+end
+
+
+
+"""
+Custom Judge:
+
+The judge will test your solution with the following code:
+
+int[] nums = [...]; // Input array
+int[] expectedNums = [...]; // The expected answer with correct length
+
+int k = removeDuplicates(nums); // Calls your implementation
+
+assert k == expectedNums.length;
+for (int i = 0; i < k; i++) {
+    assert nums[i] == expectedNums[i];
+}
+If all assertions pass, then your solution will be accepted.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,1,2,2,3]
+Output: 5, nums = [1,1,2,2,3,_]
+Explanation: Your function should return k = 5, with the first five elements of nums being 1, 1, 2, 2 and 3 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+Example 2:
+
+Input: nums = [0,0,1,1,1,1,2,3,3]
+Output: 7, nums = [0,0,1,1,2,3,3,_,_]
+Explanation: Your function should return k = 7, with the first seven elements of nums being 0, 0, 1, 1, 2, 3 and 3 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+ 
+
+Constraints:
+
+1 <= nums.length <= 3 * 104
+-104 <= nums[i] <= 104
+nums is sorted in non-decreasing order.
+
+"""
